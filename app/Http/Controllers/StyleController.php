82a5bc012ddbbdfc785c6e52b930ac34c188d4b5
Rui Marques <a.marques1967@gmail.com>
2024-03-25 21:52:37 +0000

    Added the create function

--- app/Http/Controllers/StyleController.php
+++ app/Http/Controllers/StyleController.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Cache;
 
 class StyleController extends Controller
 {
-    function index()
+    public function index()
     {
         $styles = Cache::rememberForever('styles', fn () => Style::all());
 
         return view('styles.index', [
             'styles' => $styles
         ]);
+    }
+
+    public function create()
+    {
+        return view('styles.create');
     }
 }
