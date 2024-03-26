1631e4f3a643e522cc1435fd7d1efd8bafe42d83
Rui Marques <a.marques1967@gmail.com>
2024-03-26 00:47:20 +0000

    Created an error msg component

---
+++ resources/views/components/form/error-msg.blade.php
@@ -0,0 +1,9 @@
+@if ($errors->any())
+    <div class="alert alert-danger pt-4">
+        <ul class="list-unstyled">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
