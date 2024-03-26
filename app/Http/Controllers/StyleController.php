0f3cbaefd751ff40c0a31cc0f0531b9e1a084915
Rui Marques <a.marques1967@gmail.com>
2024-03-26 00:45:26 +0000

    Added the CRUD functions

--- app/Http/Controllers/StyleController.php
+++ app/Http/Controllers/StyleController.php
@@ -21,4 +21,52 @@ class StyleController extends Controller
     {
         return view('styles.create');
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'style' => 'required|unique:styles'
+        ]);
+
+        $newStyle = Style::create($validatedData);
+
+        if (Cache::has('styles')) {
+            Cache::forget('styles');
+        }
+
+        return redirect('/styles')->with('success', "{$newStyle->style} was created with success");
+    }
+
+    public function edit(Style $style)
+    {
+        return view('styles.edit', [
+            'style' => $style
+        ]);
+    }
+
+    public function update(Style $style)
+    {
+        $validatedData = request()->validate([
+            'style' => 'required|unique:styles'
+        ]);
+
+        $style->update($validatedData);
+
+        if (Cache::has('styles')) {
+            Cache::forget('styles');
+        }
+
+        return redirect('/styles')->with('success', "{$style->style} was updated with success");
+    }
+
+    public function destroy(Style $style)
+    {
+        $style->delete();
+
+        if (Cache::has('styles')) {
+            Cache::forget('styles');
+        }
+
+        return back()->with('success', 'Style deleted with success');
+    }
 }
