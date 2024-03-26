91c5e86650e2ac08847b1b446c67f2b3679c0a77
Rui Marques <a.marques1967@gmail.com>
2024-03-26 01:10:28 +0000

    Applied the new components to the view

--- resources/views/styles/create.blade.php
+++ resources/views/styles/create.blade.php
@@ -1,26 +1,14 @@
 <x-layout>
     <div class="card">
         <div class="card-header">
-            <h1 class="card-title">Criar estilo</h1>
+            <h1 class="card-title">Create Style</h1>
         </div>
         <div class="card-body">
-            <div class="col-md-6">
-                <form action="/styles" method="post">
-                    @csrf
-
-                    <x-form.error-msg />
-
-                    <div class="mb-3">
-                        <label for="style" class="form-label">Style Name</label>
-                        <input type="text" class="form-control @error('style') is-invalid @enderror" id="style"
-                            name="style" value="{{ old('style') }}">
-                    </div>
-                    <div class="mb-3">
-                        <button type="submit" class="btn btn-success">Create</button>
-                    </div>
-                </form>
-            </div>
-
+            <x-form.form endpoint="/styles" method="post">
+                <div class="col-md-6">
+                    <x-form.input name="style" label="Style Name" value="{{ old('style') }}" />
+                </div>
+            </x-form.form>
         </div>
     </div>
 </x-layout>
--- resources/views/styles/edit.blade.php
+++ resources/views/styles/edit.blade.php
@@ -4,24 +4,11 @@
             <h1 class="card-title">Editar estilo</h1>
         </div>
         <div class="card-body">
-            <div class="col-md-6">
-                <form action="/styles/{{ $style->id }}" method="post">
-                    @csrf
-                    @method('put')
-
-                    <x-form.error-msg />
-
-                    <div class="mb-3">
-                        <label for="style" class="form-label">Style Name</label>
-                        <input type="text" class="form-control @error('style') is-invalid @enderror" id="style"
-                            name="style" value="{{ old('style') ?? $style->style }}">
-                    </div>
-                    <div class="mb-3">
-                        <button type="submit" class="btn btn-success">Edit</button>
-                    </div>
-                </form>
-            </div>
-
+            <x-form.form endpoint="/styles/{{ $style->id }}" method="put">
+                <div class="col-md-6">
+                    <x-form.input name="style" label="Style Name" value="{{ old('style') ?? $style->style }}" />
+                </div>
+            </x-form.form>
         </div>
     </div>
 </x-layout>
