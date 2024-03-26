2f33646a63cc5672d395f703c50ad46d381e279a
Rui Marques <a.marques1967@gmail.com>
2024-03-26 01:10:01 +0000

    Created a form component

---
+++ resources/views/components/form/form.blade.php
@@ -0,0 +1,15 @@
+<form action="{{ $endpoint }}" method="POST">
+    @csrf
+
+    @unless ($method === 'post')
+        @method($method)
+    @endunless
+
+    <x-form.error-msg />
+
+    {{ $slot }}
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-success">{{ $method === 'put' ? 'Edit' : 'Create' }}</button>
+    </div>
+</form>
