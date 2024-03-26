cdbefd748e2cba7f243306b04d52395d84686df1
Rui Marques <a.marques1967@gmail.com>
2024-03-26 01:40:41 +0000

    Created every CRUD function

--- app/Http/Controllers/BreweryController.php
+++ app/Http/Controllers/BreweryController.php
@@ -8,12 +8,65 @@ use Illuminate\Support\Facades\Cache;
 
 class BreweryController extends Controller
 {
-    function index()
+    public function index()
     {
         $breweries = Cache::rememberForever('breweries', fn () => Brewery::all());
 
         return view('breweries.index', [
             'breweries' => $breweries
         ]);
+    }
+
+    public function create()
+    {
+        return view('breweries.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'country' => 'required'
+        ]);
+
+        $newBrewery = Brewery::create($validatedData);
+
+        if (Cache::has('breweries')) {
+            Cache::forget('breweries');
+        }
+
+        return redirect('/breweries')->with('success', "{$newBrewery->name} was created with success");
+    }
+
+    public function edit(Brewery $brewery)
+    {
+        return view('breweries.edit', ['brewery' => $brewery]);
+    }
+
+    public function update(Brewery $brewery)
+    {
+        $validatedData = request()->validate([
+            'name' => 'required',
+            'country' => 'required'
+        ]);
+
+        $brewery->update($validatedData);
+
+        if (Cache::has('breweries')) {
+            Cache::forget('breweries');
+        }
+
+        return redirect('/breweries')->with('success', "{$brewery->name} was updated with success");
+    }
+
+    public function destroy(Brewery $brewery)
+    {
+        $brewery->delete();
+
+        if (Cache::has('breweries')) {
+            Cache::forget('breweries');
+        }
+
+        return back()->with('success', 'Brewery deleted with success');
     }
 }
