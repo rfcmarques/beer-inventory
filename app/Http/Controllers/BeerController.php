a4f8ecb6f5f7b59eadd9e8026b7ba50ff8ce03e1
Rui Marques <a.marques1967@gmail.com>
2024-03-27 01:18:02 +0000

    Added all CRUD functions

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Beer;
+use App\Models\Brewery;
+use App\Models\Style;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -10,11 +12,74 @@ class BeerController extends Controller
 {
     public function index()
     {
-
         $beers = Cache::rememberForever('beers', fn () => Beer::all());
 
         return view('beers.index', [
             'beers' => $beers,
         ]);
+    }
+
+    public function create()
+    {
+        return view('beers.create', [
+            'styles' => Style::select('id', 'name')->get(),
+            'breweries' => Brewery::select('id', 'name')->get()
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required',
+            'brewery_id' => 'required',
+            'style_id' => 'required',
+            'abv' => 'required'
+        ]);
+
+        $newBeer = Beer::create($validateData);
+
+        if (Cache::has('beers')) {
+            Cache::forget('beers');
+        }
+
+        return redirect('/beers')->with('success', "{$newBeer->name} was created with success");
+    }
+
+    public function edit(Beer $beer)
+    {
+        return view('beers.edit', [
+            'beer' => $beer,
+            'styles' => Style::select('id', 'name')->get(),
+            'breweries' => Brewery::select('id', 'name')->get()
+        ]);
+    }
+
+    public function update(Beer $beer)
+    {
+        $validateData = request()->validate([
+            'name' => 'required',
+            'brewery_id' => 'required',
+            'style_id' => 'required',
+            'abv' => 'required'
+        ]);
+
+        $beer->update($validateData);
+
+        if (Cache::has('beers')) {
+            Cache::forget('beers');
+        }
+
+        return redirect('/beers')->with('success', "{$beer->name} was updated with success");
+    }
+
+    public function destroy(Beer $beer)
+    {
+        $beer->delete();
+
+        if (Cache::has('beers')) {
+            Cache::forget('beers');
+        }
+
+        return back()->with('success', 'Beer deleted with success');
     }
 }
