bd2cf2dfb79b7e04f992d06a983793c059595cc9
Rui Marques <a.marques1967@gmail.com>
2024-03-27 01:19:18 +0000

    Changed the way that attributes work

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,5 @@
 <div class="mb-3">
     <label for="{{ $name }}" class="form-label">{{ $label }}</label>
-    <input type="text" class="form-control @error($name) is-invalid @enderror" {{ $attributes->except('label') }}>
+    <input type="{{ $type ?? 'text' }}" id="{{ $name }}" name="{{ $name }}"
+        class="form-control @error($name) is-invalid @enderror" value="{{ $value }}" />
 </div>
