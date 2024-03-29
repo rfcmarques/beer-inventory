4a9c40472464831dc1811c5d5d33446a261ea3bf
Rui Marques <a.marques1967@gmail.com>
2024-03-29 02:20:43 +0000

    Created the container component

---
+++ resources/views/components/container.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="d-flex flex-row align-items-center mb-3">
+        <h1 class="me-auto">{{ $title }}</h1>
+        @if ($button)
+            <div class="col-2 d-grid">
+                <a href="{{ $button['endpoint'] }}" class="btn btn-primary" role="button">{{ $button['text'] }}</a>
+            </div>
+        @endif
+    </div>
+
+    {{ $slot }}
+
+</div>
