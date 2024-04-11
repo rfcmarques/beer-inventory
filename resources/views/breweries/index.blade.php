65c80f58fcd83ca2efd8c137c2d8a98adc4b83d5
Rui Marques <a.marques1967@gmail.com>
2024-04-11 21:08:47 +0100

    Changed index to start using the brewery card component

--- resources/views/breweries/index.blade.php
+++ resources/views/breweries/index.blade.php
@@ -1,45 +1,12 @@
 <x-layout>
     <x-success-msg />
 
-    <div class="card">
-        <div class="card-header">
-            <div class="d-flex">
-                <h1 class="card-title me-auto">Breweries List</h1>
-                <a href="/breweries/create" class="btn btn-primary">Add</a>
-            </div>
+    <x-container title="Breweries" :button="['endpoint' => '/breweries/add', 'text' => 'Create']">
+        <div class="row mt-4">
+            @foreach ($breweries as $brewery)
+                <x-brewery-card :brewery="$brewery" />
+            @endforeach
         </div>
-        <div class="card-body">
-            <div class="row">
-                @foreach ($breweries as $brewery)
-                    <div class="col-4 mb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center">
-                                    <div class="me-5">
-                                        <i class="fa-solid fa-font-awesome fa-2xl"></i>
-                                    </div>
-                                    <div>
-                                        <h4 class="card-title">{{ $brewery->name }}</h4>
-                                        <p>{{ $brewery->country }}</p>
-                                    </div>
-                                    <div class="d-flex flex-column align-items-center ms-auto">
-                                        <a href="/breweries/{{ $brewery->id }}/edit">
-                                            <i class="fa-solid fa-pen-to-square text-primary"></i>
-                                        </a>
-                                        <form action="/breweries/{{ $brewery->id }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <button class="btn btn-link text-decoration-none" type="submit">
-                                                <i class="fa-solid fa-trash text-danger"></i>
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    </x-container>
+
 </x-layout>
