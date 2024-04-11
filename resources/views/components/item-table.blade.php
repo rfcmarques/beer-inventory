d21d0d3a958699b1803c735e78bb7541fe929809
Rui Marques <a.marques1967@gmail.com>
2024-04-11 23:07:35 +0100

    Created the items table

---
+++ resources/views/components/item-table.blade.php
@@ -0,0 +1,27 @@
+<table class="table table-striped table-hover">
+    <thead>
+        <th>Beer</th>
+        <th>Style</th>
+        <th>Brewery</th>
+        <th>Container</th>
+        <th>Best By</th>
+    </thead>
+    <tbody class="table-group-divider">
+        @foreach ($items as $item)
+            <tr>
+
+                <td>
+                    <a class="text-decoration-none fw-semibold text-dark" href="/items/{{ $item->id }}/edit">
+                        {{ $item->beer->name }}
+                    </a>
+                </td>
+                <td>{{ $item->beer->style->name }}</td>
+                <td>{{ $item->beer->brewery->name }}</td>
+                <td>{{ $item->container }}</td>
+                <td title="{{ $item->expiration_date->format('Y-m-d') }}">
+                    {{ $item->expiration_date->diffForHumans() }}
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
