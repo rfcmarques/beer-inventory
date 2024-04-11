ed90ff554e10957c4b463f69287dac023c351d7a
Rui Marques <a.marques1967@gmail.com>
2024-04-11 22:30:14 +0100

    Changed the button text in breweries index

--- resources/views/breweries/index.blade.php
+++ resources/views/breweries/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-success-msg />
 
-    <x-container title="Breweries" :button="['endpoint' => '/breweries/add', 'text' => 'Create']">
+    <x-container title="Breweries" :button="['endpoint' => '/breweries/add', 'text' => 'Add']">
         <div class="row mt-4">
             @foreach ($breweries as $brewery)
                 <x-brewery-card :brewery="$brewery" />
