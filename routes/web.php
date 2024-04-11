ac3a118605197c756eb4fb036c6f2ea7acaafdd0
Rui Marques <a.marques1967@gmail.com>
2024-04-11 19:46:31 +0100

    Changed the resources to an alphabetical order and turned home endpoint into a view

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,12 @@ use App\Http\Controllers\BeerController;
 use App\Http\Controllers\BreweryController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\StyleController;
-use App\Models\Brewery;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('home.index');
-});
+Route::view('/', 'home.index');
 
 Route::resource('beers', BeerController::class)->except('show');
-Route::resource('styles', StyleController::class)->except('show');
 Route::resource('breweries', BreweryController::class)->except('show');
 Route::resource('items', ItemController::class);
+Route::resource('styles', StyleController::class)->except('show');
