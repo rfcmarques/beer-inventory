71bff9aabfcbdff9012745b114ac53de5f5a5cb4
Rui Marques <a.marques1967@gmail.com>
2024-05-01 19:13:49 +0100

    Separated the input and the label to independent components

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,5 +1,4 @@
-<div class="mb-3">
-    <label for="{{ $name }}" class="form-label">{{ $label }}</label>
-    <input type="{{ $type ?? 'text' }}" id="{{ $name }}" name="{{ $name }}"
-        class="form-control @error($name) is-invalid @enderror" value="{{ $value }}" />
-</div>
+@props(['type' => 'text', 'name', 'value'])
+
+<input type="{{ $type ?? 'text' }}" id="{{ $name }}" name="{{ $name }}"
+    class="form-control @error($name) is-invalid @enderror" value="{{ $value }}" />
