76c8c609f109cf13500f22185158047a8ad2d23c
Rui Marques <a.marques1967@gmail.com>
2024-05-01 18:46:47 +0100

    Started to use the new components

--- resources/views/styles/create.blade.php
+++ resources/views/styles/create.blade.php
@@ -1,14 +1,11 @@
 <x-layout>
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Create Style</h1>
-        </div>
-        <div class="card-body">
-            <x-form.form endpoint="/styles" method="post">
+    <x-container title="Create Style">
+        <x-card>
+            <x-form.form endpoint="/styles">
                 <div class="col-md-6">
                     <x-form.input name="name" label="Style Name" value="{{ old('name') }}" />
                 </div>
             </x-form.form>
-        </div>
-    </div>
+        </x-card>
+    </x-container>
 </x-layout>
--- resources/views/styles/edit.blade.php
+++ resources/views/styles/edit.blade.php
@@ -1,14 +1,11 @@
 <x-layout>
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Editar estilo</h1>
-        </div>
-        <div class="card-body">
+    <x-container title="Edit Style">
+        <x-card>
             <x-form.form endpoint="/styles/{{ $style->id }}" method="put">
                 <div class="col-md-6">
                     <x-form.input name="name" label="Style Name" value="{{ old('name') ?? $style->name }}" />
                 </div>
             </x-form.form>
-        </div>
-    </div>
+        </x-card>
+    </x-container>
 </x-layout>
