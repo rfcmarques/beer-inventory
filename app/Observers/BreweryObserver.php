4e12c68083a294c0b20c126ac5f70f82c5e03386
Rui Marques <a.marques1967@gmail.com>
2024-05-02 00:31:05 +0100

    Added more observer rules to Style and Breweries

--- app/Observers/BreweryObserver.php
+++ app/Observers/BreweryObserver.php
@@ -10,15 +10,21 @@ class BreweryObserver
     public function created(Brewery $brewery): void
     {
         Cache::forget('breweries');
+        Cache::forget('beers');
+        Cache::forget('items');
     }
 
     public function updated(Brewery $brewery): void
     {
         Cache::forget('breweries');
+        Cache::forget('beers');
+        Cache::forget('items');
     }
 
     public function deleted(Brewery $brewery): void
     {
         Cache::forget('breweries');
+        Cache::forget('beers');
+        Cache::forget('items');
     }
 }
