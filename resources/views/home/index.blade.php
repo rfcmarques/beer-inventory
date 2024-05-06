4217fbd40b84803fb95294779c2ab3fc99fd9912
Rui Marques <a.marques1967@gmail.com>
2024-05-06 16:53:31 +0100

    Added some stats so I can remember later

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
-    <h1>Estatísticas</h1>
+    <h1>Stats</h1>
+
+    <h3>Items</h3>
+    <p>You have a total of {{ \App\Models\Item::all()->count() }} different items in your inventory</p>
+    <p>The following items will reach their best before date soon</p>
+    <ul>
+        <li>ABC</li>
+        <li>ABC</li>
+        <li>ABC</li>
+        <li>ABC</li>
+        <li>ABC</li>
+    </ul>
+
+    <h3>Beers</h3>
+    <p>You have a total of {{ \App\Models\Beer::all()->count() }} different beers</p>
+
+    <h3>Breweries</h3>
+    <p>You have a total of {{ \App\Models\Brewery::all()->count() }} different breweries</p>
+
+    <h3>Styles</h3>
+    <p>You have a total of {{ \App\Models\Style::all()->count() }} different styles</p>
 </x-layout>
