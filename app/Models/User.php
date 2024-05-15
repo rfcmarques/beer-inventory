f737c4e73522783646f1a819cc9f4127658b47e2
Rui Marques <a.marques1967@gmail.com>
2024-05-16 00:33:06 +0100

    Created the user model and the corresponding migration

---
+++ app/Models/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+}
