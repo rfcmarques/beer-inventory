4e584e0b31efc13c31bf7d1e1213d6e5db1acdbc
Rui Marques <a.marques1967@gmail.com>
2024-05-16 00:33:28 +0100

    Created the login routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BeerController;
 use App\Http\Controllers\BreweryController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\StyleController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,5 +12,11 @@ Route::view('/', 'home.index');
 
 Route::resource('beers', BeerController::class)->except('show');
 Route::resource('breweries', BreweryController::class)->except('show');
-Route::resource('items', ItemController::class);
+Route::resource('items', ItemController::class)->except('show');
 Route::resource('styles', StyleController::class)->except('show');
+
+Route::controller(SessionController::class)->group(function () {
+    Route::get('/login', 'create')->middleware('guest');
+    Route::post('/login', 'store')->middleware('guest');
+    Route::delete('/login', 'destroy')->middleware('auth');
+});
