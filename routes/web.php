eaed1d15297b7d12ee328dc734bcd1ec5d4bcc8c
Rui Marques <a.marques1967@gmail.com>
2024-05-21 01:23:40 +0100

    Created a policy for Breweries and applied to the corresponding routes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,30 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home.index');
 
 Route::resource('beers', BeerController::class)->except('show');
-Route::resource('breweries', BreweryController::class)->except('show');
+
+Route::controller(BreweryController::class)->group(function () {
+    Route::get('/breweries', 'index');
+
+    Route::get('/breweries/create', 'create')
+        ->can('create', 'App\Models\Brewery');
+
+    Route::post('/breweries', 'store');
+
+    Route::get('/breweries/{brewery}/edit', 'edit')
+        ->can('edit', 'brewery');
+
+    Route::put('/breweries/{brewery}', 'update')
+        ->can('update', 'brewery');
+
+    Route::delete('/breweries/{brewery}', 'delete')
+        ->can('delete', 'brewery');
+});
+
 Route::resource('items', ItemController::class)->except('show');
 Route::resource('styles', StyleController::class)->except('show');
 
 Route::controller(SessionController::class)->group(function () {
-    Route::get('/login', 'create')->middleware('guest');
+    Route::get('/login', 'create')->middleware('guest')->name('login');
     Route::post('/login', 'store')->middleware('guest');
     Route::delete('/login', 'destroy')->middleware('auth');
 });
