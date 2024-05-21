75ec2605c3d530d2c76ae9137b3ec1e326c6fdee
Rui Marques <a.marques1967@gmail.com>
2024-05-21 20:49:41 +0100

    Changed the way the authorization works in each FormRequest

--- app/Http/Requests/UpdateBreweryRequest.php
+++ app/Http/Requests/UpdateBreweryRequest.php
@@ -11,7 +11,7 @@ class UpdateBreweryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->user()->admin;
     }
 
     /**
--- app/Http/Requests/UpdateItemRequest.php
+++ app/Http/Requests/UpdateItemRequest.php
@@ -11,7 +11,7 @@ class UpdateItemRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->user()->admin;
     }
 
     /**
