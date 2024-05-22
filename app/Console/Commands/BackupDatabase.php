684904eec8ef7fcffb09499b5473f457a7b14ccb
Rui Marques <a.marques1967@gmail.com>
2024-05-22 03:12:21 +0100

    Added filename and table options to the command and refactored it

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -14,7 +14,9 @@ class BackupDatabase extends Command
      *
      * @var string
      */
-    protected $signature = 'db:backup';
+    protected $signature = 'db:backup
+                            {--filename= : Set a custom filename}
+                            {--table= : Backup a specific table}';
 
     /**
      * The console command description.
@@ -42,23 +44,61 @@ class BackupDatabase extends Command
      */
     public function handle()
     {
-        $backupData = [];
+        $backupData = $this->option('table')
+            ? $this->getTableData($this->option('table'))
+            : $this->getDatabaseData();
+
+        $filename = $this->setFilename();
+
+        $this->storeBackup($backupData, $filename);
+
+        $this->sendSuccessMessage($filename);
+    }
+
+    /**
+     * Returns all the data from the database
+     * stored in an array
+     * 
+     * @return array
+     */
+    private function getDatabaseData(): array
+    {
+        $data = [];
 
         foreach ($this->tables as $tableName) {
-            if ($this->tableExists($tableName)) {
-                $data = DB::table($tableName)->get();
-                $backupData[$tableName] = $data;
-            }
+            $data[$tableName] = $this->getTableData($tableName);
         }
 
+        return $data;
+    }
+
+    /**
+     * @param string $tableName
+     * @return \Illuminate\Support\Collection|void
+     */
+    private function getTableData($tableName)
+    {
+        if ($this->tableExists($tableName)) {
+            return DB::table($tableName)->get();
+        } else {
+            $this->sendErrorMessage($tableName);
+        }
+    }
+
+    /**
+     * Stores the given data into a JSON file with the given filename
+     * 
+     * @param mixed $backupData
+     * @param string $filename
+     * @return void
+     */
+    private function storeBackup($backupData, $filename): void
+    {
         // Convert data to JSON
         $jsonBackup = json_encode($backupData, JSON_PRETTY_PRINT);
 
         // Save JSON to file
-        $filename = 'backup_' . date('Y_m_d_H_i_s') . '.json';
         Storage::disk('backups')->put($filename, $jsonBackup);
-
-        $this->provideFeedback($filename);
     }
 
     /**
@@ -72,7 +112,13 @@ class BackupDatabase extends Command
         return Schema::hasTable($tableName);
     }
 
-    private function provideFeedback($filename): void
+    /**
+     * Write the success message
+     * 
+     * @param string $filename
+     * @return void
+     */
+    private function sendSuccessMessage($filename): void
     {
         $this->line('');
 
@@ -84,5 +130,41 @@ class BackupDatabase extends Command
         $this->info("   \033[44m INFO \033[0m Backup is stored at \033[1m[<href=file://$backupPath>{$backupPath}]\033[0m</>");
 
         $this->line('');
+    }
+
+    /**
+     * Writes an error message and ends the command
+     * 
+     * @param string $tableName
+     * @return void
+     */
+    private function sendErrorMessage($tableName): void
+    {
+        $this->line('');
+
+        $this->info("   \033[41m ERROR \033[0m Table '$tableName' does not exist!");
+
+        $this->line('');
+
+        exit;
+    }
+
+    /**
+     * Define the file name based on the option,
+     * if there is no option defined uses the default
+     * 
+     * @return string
+     */
+    private function setFilename(): string
+    {
+        if ($this->option('filename')) {
+            return $this->option('filename') . '.json';
+        }
+
+        if ($this->option('table')) {
+            return $this->option('table') . '_backup_' . date('Y_m_d_H_i_s') . '.json';
+        }
+
+        return 'backup_' . date('Y_m_d_H_i_s') . '.json';
     }
 }
