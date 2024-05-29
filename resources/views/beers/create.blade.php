f77e3b14672e42efba5bfcfa3d9c229e33df6c4f
Rui Marques <a.marques1967@gmail.com>
2024-05-29 18:21:16 +0100

    Changed to use the new form components

--- resources/views/beers/create.blade.php
+++ resources/views/beers/create.blade.php
@@ -1,37 +1,33 @@
 <x-layout>
     <x-container title="Create Beer">
         <x-card>
-            <x-form.form endpoint="/beers">
+            <x-form.form action="/beers">
                 <div class="row">
-                    <x-form.field>
-                        <x-form.label for="name">Name</x-form.label>
-                        <x-form.input name="name" value="{{ old('name') }}" />
-                    </x-form.field>
+                    <div class="col-md-6 mb-3">
+                        <x-form.input label="Name" name="name" />
+                    </div>
 
-                    <x-form.field>
-                        <x-form.label for="brewery_id">Brewery</x-form.label>
-                        <x-form.select name="brewery_id">
-                            @foreach ($breweries as $key => $brewery)
-                                <x-form.option value="{{ $brewery->id }}" text="{{ $brewery->name }}"
+                    <div class="col-md-6 mb-3">
+                        <x-form.select label="Brewery" name="brewery_id">
+                            @foreach ($breweries as $brewery)
+                                <x-form.option :value="$brewery->id" :text="$brewery->name"
                                     selectedValue="{{ old('brewery_id') }}" />
                             @endforeach
                         </x-form.select>
-                    </x-form.field>
+                    </div>
 
-                    <x-form.field>
-                        <x-form.label for="style_id">Style</x-form.label>
-                        <x-form.select name="style_id">
-                            @foreach ($styles as $key => $style)
-                                <x-form.option value="{{ $style->id }}" text="{{ $style->name }}"
+                    <div class="col-md-6 mb-3">
+                        <x-form.select label="Style" name="style_id">
+                            @foreach ($styles as $style)
+                                <x-form.option :value="$style->id" :text="$style->name"
                                     selectedValue="{{ old('style_id') }}" />
                             @endforeach
                         </x-form.select>
-                    </x-form.field>
+                    </div>
 
-                    <x-form.field>
-                        <x-form.label for="abv">ABV (%)</x-form.label>
-                        <x-form.input type="number" name="abv" value="{{ old('abv') }}" />
-                    </x-form.field>
+                    <div class="col-md-6 mb-3">
+                        <x-form.input label="ABV (%)" type="number" name="abv" />
+                    </div>
                 </div>
             </x-form.form>
         </x-card>
--- resources/views/styles/edit.blade.php
+++ resources/views/styles/edit.blade.php
@@ -1,11 +1,12 @@
 <x-layout>
     <x-container title="Edit Style">
         <x-card>
-            <x-form.form endpoint="/styles/{{ $style->id }}" method="put">
-                <x-form.field>
-                    <x-form.label for="name">Style Name</x-form.label>
-                    <x-form.input name="name" value="{{ old('name') ?? $style->name }}" />
-                </x-form.field>
+            <x-form.form action="/styles/{{ $style->id }}" method="put">
+
+                <div class="col-md-6 mb-3">
+                    <x-form.input label="Style Name" name="name" :value="$style->name" />
+                </div>
+
             </x-form.form>
         </x-card>
     </x-container>
