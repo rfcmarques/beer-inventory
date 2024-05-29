6b307d8a27244dc22fcf45b7e89111775bc1dd03
Rui Marques <a.marques1967@gmail.com>
2024-05-29 15:25:10 +0100

    Applied some changes regarding the image URL and the permissions for the buttons

--- resources/views/components/brewery-card.blade.php
+++ resources/views/components/brewery-card.blade.php
@@ -7,24 +7,29 @@
                     <p>{{ $brewery->country }}</p>
                 </div>
                 <div class="col">
-                    <img src="{{ Storage::url($brewery->logo) ?? '/imgs/hop.svg' }}"
-                        class="img-fluid object-fit-contain" />
+                    @php
+                        $logoUrl = $brewery->logo ? Storage::url($brewery->logo) : '/imgs/hop.svg';
+                    @endphp
+
+                    <img src="{{ $logoUrl }}" class="img-fluid object-fit-contain" />
                 </div>
             </div>
         </div>
-        <div class="card-footer border-0 p-0 mx-0 text-bg-light">
-            <div class="d-grid">
-                <div class="btn-group" role="group">
-                    <button class="btn btn-light border-top border-end text-danger" form="delete-form">
-                        Delete
-                    </button>
-                    <a href="/breweries/{{ $brewery->id }}/edit"
-                        class="btn btn-light border-top border-start text-primary">
-                        Edit
-                    </a>
+        @can('update', $brewery)
+            <div class="card-footer border-0 p-0 mx-0 text-bg-light">
+                <div class="d-grid">
+                    <div class="btn-group" role="group">
+                        <button class="btn btn-light border-top border-end text-danger text-opacity-75" form="delete-form">
+                            Delete
+                        </button>
+                        <a href="/breweries/{{ $brewery->id }}/edit"
+                            class="btn btn-light border-top border-start text-primary text-opacity-75">
+                            Edit
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endcan
     </div>
 
     <form method="POST" action="/breweries/{{ $brewery->id }}" id="delete-form" class="d-none">
