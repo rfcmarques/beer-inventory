56f671b50950eded6188e3775e94bfec75adbd80
Rui Marques <a.marques1967@gmail.com>
2024-06-30 17:55:48 +0100

    Added local scoped queries and the corresponding documentation

--- app/Models/Beer.php
+++ app/Models/Beer.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use App\Observers\BeerObserver;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[ObservedBy(BeerObserver::class)]
 class Beer extends Model
@@ -16,18 +19,72 @@ class Beer extends Model
 
     protected $fillable = ['brewery_id', 'style_id', 'name', 'abv'];
 
-    public function style()
+    public function style(): BelongsTo
     {
         return $this->belongsTo(Style::class);
     }
 
-    public function brewery()
+    public function brewery(): BelongsTo
     {
         return $this->belongsTo(Brewery::class);
     }
 
-    public function items()
+    public function items(): HasMany
     {
         return $this->hasMany(Item::class);
+    }
+
+    /**
+     * Scope a query to only include 
+     * beers with available items.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeAvailable(Builder $query): Builder
+    {
+        return $query->whereHas('items', fn ($query) => $query->available());
+    }
+
+    /**
+     * Scope a query to only include 
+     * beers with consumed items.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeConsumed(Builder $query): Builder
+    {
+        return $query->whereHas('items', fn ($query) => $query->consumed());
+    }
+
+    /**
+     * Scope a query to include 
+     * the count of available items 
+     * for each beer.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeQuantityAvailable(Builder $query): Builder
+    {
+        return $query->withCount([
+            'items as quantity_available' => fn ($query) => $query->available()
+        ]);
+    }
+
+    /**
+     * Scope a query to include t
+     * he count of consumed items 
+     * for each beer.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeQuantityConsumed(Builder $query): Builder
+    {
+        return $query->withCount([
+            'items as quantity_consumed' => fn ($query) => $query->consumed()
+        ]);
     }
 }
