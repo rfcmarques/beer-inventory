4d48c8d3c0acc6a1d605a39df081d2bd7fdf305d
Rui Marques <a.marques1967@gmail.com>
2024-06-30 02:48:52 +0100

    Added some local scopes to facilitate fetching data

--- app/Models/Style.php
+++ app/Models/Style.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use App\Observers\StyleOberserver;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[ObservedBy([StyleOberserver::class])]
 class Style extends Model
@@ -14,8 +17,48 @@ class Style extends Model
 
     protected $fillable = ['name'];
 
-    public function beers()
+    public function beers(): HasMany
     {
         return $this->hasMany(Beer::class);
+    }
+
+    public function scopeWithAvailableItems(Builder $query): Builder
+    {
+        return $query->whereHas('beers.items', fn ($query) => $query->available());
+    }
+
+    public function scopeWithConsumedItems(Builder $query)
+    {
+        return $query->whereHas('beers.items', fn ($query) => $query->consumed());
+    }
+
+    public function scopeWithAvailableBeersCount(Builder $query): Builder
+    {
+        return $query->withCount([
+            'beers as available_beers' => fn ($query) => $query->available()
+        ]);
+    }
+
+    public function scopeWithConsumedBeersCount(Builder $query): Builder
+    {
+        return $query->withCount([
+            'beers as consumed_beers' => fn ($query) => $query->consumed()
+        ]);
+    }
+
+    public function scopeWithAvailableItemsCount(Builder $query): Collection
+    {
+        return $query->with(['beers' => fn ($query) => $query->quantityAvailable()])
+            ->get()->each(function ($style) {
+                $style->available_items = $style->beers->sum('quantity_available');
+            });
+    }
+
+    public function scopeWithConsumedItemsCount(Builder $query): Collection
+    {
+        return $query->with(['beers' => fn ($query) => $query->quantityConsumed()])
+            ->get()->each(function ($style) {
+                $style->consumed_items = $style->beers->sum('quantity_consumed');
+            });
     }
 }
