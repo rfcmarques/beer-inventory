46184a80f5ed239741dd47672496da2feeaad5aa
Rui Marques <a.marques1967@gmail.com>
2024-06-30 19:42:35 +0100

    Comments for testing

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,6 +5,8 @@
     <x-container title="Inventory" :button="['endpoint' => '/items/create', 'text' => 'Add']">
         <x-card class="border shadow py-3 px-4">
             <x-item-table :items="$items" />
+
+            {{-- {{ $items->onEachSide(1)->links() }} --}}
         </x-card>
     </x-container>
 
