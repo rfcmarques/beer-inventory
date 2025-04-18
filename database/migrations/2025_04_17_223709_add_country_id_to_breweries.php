973e0c180fd152d3c61a4137498f829989f80e51
Rui Marques <a.marques1967@gmail.com>
2025-04-18 01:13:04 +0100

    feat: add countries table and update breweries schema

---
+++ database/migrations/2025_04_17_223709_add_country_id_to_breweries.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Country;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('breweries', function (Blueprint $table) {
+            $table->foreignIdFor(Country::class)
+                ->nullable()
+                ->after('name')
+                ->constrained()
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('breweries', function (Blueprint $table) {
+            //
+        });
+    }
+};
