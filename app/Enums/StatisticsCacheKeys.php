84f5fea3f355442171248fa179a57b6d284ea2e4
Rui Marques <rmarques@cegid.com>
2025-05-06 17:57:34 +0100

    feat: added missing cache keys

--- app/Enums/StatisticsCacheKeys.php
+++ app/Enums/StatisticsCacheKeys.php
@@ -12,8 +12,15 @@ enum StatisticsCacheKeys: string
     case BREWERIES_AVAILABLE = 'breweries-available';
     case STYLES_CONSUMED = 'styles-consumed';
     case STYLES_AVAILABLE = 'styles-available';
+    case COUNTRIES_AVAILABLE = 'countries-available';
+    case COUNTRIES_CONSUMED = 'countries-consumed';
+    case LITERS_CONSUMED = 'liters-consumed';
+    case LITERS_AVAILABLE = 'liters-available';
     case MEAN_TIME_TO_CONSUME = 'mean-time-to-consume';
     case LAST_BEERS_CONSUMED = 'last-beers-consumed';
+    case ITEMS_CONSUMED_PER_WEEK = 'items-consumed-per-week';
+    case ITEMS_CONSUMED_PER_MONTH = 'items-consumed-per-month';
+    case ITEMS_CONSUMED_PER_YEAR = 'items-consumed-per-year';
     case EXPIRING_BEERS = 'expiring-beers';
     case TOP_5_AVAILABLE_BREWERIES = 'top-5-available-breweries';
     case TOP_5_CONSUMED_BREWERIES = 'top-5-consumed-breweries';
