d7a3f35d918100d16686b21f5943ebb5ed4e2264
Rui Marques <rmarques@cegid.com>
2025-05-06 17:58:47 +0100

    style: changed the timing of the carousel

--- resources/views/components/statistics/card-stats-carousel.blade.php
+++ resources/views/components/statistics/card-stats-carousel.blade.php
@@ -1,6 +1,6 @@
 <x-card class="border-0 border-bottom border-5 {{ $border }} rounded-2 shadow">
     <h5 class="card-title">{{ $title }}</h5>
-    <div class="carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
+    <div class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
         <div class="carousel-inner">
             @foreach ($carousels as $title => $carousel)
                 <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
