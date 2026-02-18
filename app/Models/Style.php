b59fd818502ac21371e102a34c502911f011e562
Rui Marques <a.marques1967@gmail.com>
2026-02-18 21:10:10 +0000

    chore: removed unnecessary comments

--- app/Models/Style.php
+++ app/Models/Style.php
@@ -12,25 +12,14 @@ use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Style extends Model
 {
-    /** @use HasFactory<\Database\Factories\StyleFactory> */
     use HasFactory;
     use HasCustomBuilder;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'srm',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
